6c7d6409ac03a87b29b9f9f24e7ed48519d86530
jatinchary <charyjatin@gmail.com>
2024-12-20 15:41:50 +0530

    Your commit message

---
+++ src/JatinCoin.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract JatinCoin is ERC20 {
+    uint256 public number;
+
+    constructor() ERC20("JatinCoin", "JC") {}
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
